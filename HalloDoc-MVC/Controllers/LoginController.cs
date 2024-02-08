a5837ff4ba5ae1e84a27cc4e50bfaff1ee58cb48
NiyatiKaneriya <niyati.kaneriya@etatvasoft.com>
2024-02-08 10:02:34 +0530

    databse integration of login page

--- HalloDoc-MVC/Controllers/LoginController.cs
+++ HalloDoc-MVC/Controllers/LoginController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HalloDoc_DAL.DataContext;
+using HalloDoc_DAL.Models;
+using HalloDoc_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using System.Data;
 
 namespace HalloDoc_Patient.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public LoginController(ApplicationDbContext context) {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -12,5 +22,20 @@ namespace HalloDoc_Patient.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult checkLogin(AspNetUser aspNetUser)
+        {
+            var user = _context.AspNetUsers.FirstOrDefault(u => (u.UserName == aspNetUser.UserName) && (u.PasswordHash == aspNetUser.PasswordHash));
+            if(user != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewData["error"] = "Invalid Id Pass";
+                return View("Index");
+            }
+        }
+
     }
 }
